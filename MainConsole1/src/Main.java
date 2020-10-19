7b2079944bf31c942af3332868d5393b47b11cc8
Teacher <teacher@mirea.ru>
2020-10-19 10:57:28 +0300

    Add unlimited hello world

--- MainConsole1/src/Main.java
+++ MainConsole1/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        while(true) {
+            System.out.println("Hello world");
+        }
     }
 
 }
